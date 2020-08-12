50bf418ff7c5a0c5887703e9c0329bfa30eb3608
dario <dario.klepoch@freenet.de>
2020-08-12 18:40:05 +0200

    made images resizeable

--- timelapse.py
+++ timelapse.py
@@ -2,6 +2,7 @@
 import datetime, time
 import requests 
 import os
+#PySide2
 
 
 
@@ -20,11 +21,12 @@ class timelapse(object):
     pictures_taken = 0
 
 
-    def __init__(self, capture_timeframe : int, url : str, path : str):
+    def __init__(self, url : str, path : str, name : str, capture_timeframe : int):
 
-        self.capture_timeframe = capture_timeframe
         self.url = url
         self.path = path
+        self.name = name
+        self.capture_timeframe = capture_timeframe
 
 
     def download_image(self):
@@ -35,4 +37,20 @@ class timelapse(object):
         _name =  "test" + "_" + str(self.pictures_taken) + ".jpg"
 
         img = requests.get(self.url, stream=True)
+
+
+        #test
+
+        label_pixmap = None
+        #url = "http://192.168.178.51/webcam/?action=snapshot"
+        #if(url == ""):
+        #    label_pixmap = QPixmap(os.path.join("img", "placeholder.png"))
+        #else:
+        #    img = requests.get(url, stream=True)
+        #    img.raw.decode_content = True
+        #    #qimg = QImage.loadFromData(img.content)
+        #    label_pixmap = QPixmap()
+        #    label_pixmap.loadFromData(QByteArray(img.raw.data))
+        #label_video_stream.setPixmap(label_pixmap)
+        #label_video_stream.setMask(label_pixmap.mask())
     
