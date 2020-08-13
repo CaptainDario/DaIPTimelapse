cf41687d8d9c502c17e89eefb010acc11b19cae9
dario <dario.klepoch@freenet.de>
2020-08-13 23:40:48 +0200

    added time lapse name to saved images

--- timelapse.py
+++ timelapse.py
@@ -72,7 +72,7 @@ class timelapse(object):
             self.label_last_image_taken.setPixmap(label_pixmap)
 
             #save the image
-            with open(os.path.join(self.path, self.name, "images", str(self.pictures_taken) + ".jpg"), "wb") as f:
+            with open(os.path.join(self.path, self.name, "images", self.name + str(self.pictures_taken) + ".jpg"), "wb") as f:
                 f.write(img.raw.data)
 
             self.pictures_taken += 1
