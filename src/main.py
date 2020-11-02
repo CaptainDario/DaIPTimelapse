2429915426ec9d9b9604fa213ebb9bc67eab7e93
CaptainDario <daapplab@gmail.com>
2020-11-02 14:28:43 +0100

    renamed ui to IO class

--- src/main.py
+++ src/main.py
@@ -1,19 +1,23 @@
 #standard
 import os, sys
-#custom
-import ui
-import about
 #PySide2
 from PySide2.QtWidgets import QApplication, QMainWindow
+#custom
+import IO
+import ui_elements
+import about
+
 
 
 def main():
     app = QApplication(sys.argv)
 
     #load the ui from file
-    window = ui.load_ui_file(os.path.join("ui", "main.ui")) 
+    window = IO.load_ui_file(os.path.join("ui", "main.ui")) 
     window.setWindowTitle(about.name + " - " + about.version)
     window.show()
+
+    main_ui = ui_elements.main_ui(window)
 
     sys.exit(app.exec_())
 
