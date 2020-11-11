4f7e77a9ad302baf9328b5d31563db35d4e6a503
dario <dario.klepoch@freenet.de>
2020-11-11 15:02:35 +0100

    added github links

--- src/about.py
+++ src/about.py
@@ -1,3 +1,14 @@
 
-name = "DaIPTimelapse"
-version = "0.3a"
+name           = "DaIPTimelapse"
+version        = "0.1"
+release_suffix = "a"
+
+full_id = " ".join([name, "v" + version + release_suffix])
+
+#Github
+repo_url           = "https://github.com/CaptainDario/DaIPTimelapse"
+pull_url           = "https://github.com/CaptainDario/DaIPTimelapse/pulls"
+latest_release_url = "https://github.com/CaptainDario/DaIPTimelapse/releases/latest"
+
+#Github API
+latest_release_api = "https://api.github.com/repos/CaptainDario/DaIPTimelapse/releases/latest"
