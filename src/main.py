b1b6c8e69a171d30b874ae2fc603b4b0b4368a24
dario <dario.klepoch@freenet.de>
2020-11-11 15:01:45 +0100

    chck for updates at start

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 #standard
 import os, sys
+import webbrowser
 from PySide2.QtCore import QFile, QTextStream
 #PySide2
 from PySide2.QtWidgets import QApplication, QMainWindow
@@ -8,6 +9,7 @@ import IO
 from main_ui import main_ui 
 import about
 import breeze_resources
+import network
 
 
 
@@ -23,9 +25,18 @@ def main():
 
     #load the ui from file
     window = IO.load_ui_file(IO.resource_path(os.path.join("ui", "main.ui"))) 
-    window.setWindowTitle(about.name + " - v" + about.version)
+    window.setWindowTitle(about.full_id)
     
     window.show()
+
+    #check for newer version
+    if(network.check_for_update()):
+        u_window = IO.load_ui_file(IO.resource_path(os.path.join("ui", "update.ui"))) 
+        u_window.setWindowTitle("Update available")
+        u_window.pushButton_download.clicked.connect(lambda : webbrowser.open(about.latest_release_url))
+        u_window.pushButton_close.clicked.connect(lambda : u_window.close())
+        u_window.show()
+        
 
     ui = main_ui(window)
 
